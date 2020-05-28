e5cbbc8608eac587b006f5b208b6a8638aec6905
Harsh rai <raiharsh88@gmail.com>
2020-05-29 01:53:03 +0530

    first tet with authorization server

--- src/app.js
+++ src/app.js
@@ -12,8 +12,26 @@ const path_to_static = path.join(__dirname, "/../public");
 console.log(path_to_static);
 app.use(express.static(path_to_static));
 
-app.get("/", (req, res) => {
+//Send the login page
+
+app.get("/auth", (req, res) => {
   res.sendFile("index.html", { root: path_to_static });
+});
+
+//Send the authrization token
+app.post("/auth", (req, res) => {
+  res.send({ res: "submitted" });
+});
+
+//get authentication request
+app.get("/auth", (req, res) => {
+  res.send({ res: "you getted auth data" });
+});
+//post authentication request;
+
+app.post("/auth", (req, res) => {
+  console.log(req.body);
+  res.send({ res: "you posted auth data" });
 });
 
 io.on("connection", (socket) => {
